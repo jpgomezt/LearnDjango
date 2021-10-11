fcd33dd05741273cdccf9cf60899b44cc2e49413
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-10-11 15:56:44 -0500

    Changed models default name and created question.index

--- mysite/polls/models/choice.py
+++ mysite/polls/models/choice.py
@@ -7,5 +7,8 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    class Meta:        
+        db_table = 'choice'
+
     def __str__(self):
         return self.choice_text
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home_view.index, name='index'),
+    path('', home_view.index, name='home.index'),
+    path('question/', question_view.index, name='question.index'),
     # ex: /polls/question/5/
-    path('question/<int:question_id>/', question_view.detail, name='detail'),
+    path('question/<int:question_id>/', question_view.detail, name='question.detail'),
     # ex: /polls/question/5/results/
-    path('question/<int:question_id>/results/', question_view.results, name='results'),
+    path('question/<int:question_id>/results/', question_view.results, name='question.results'),
     # ex: /polls/question/5/vote/
-    path('question/<int:question_id>/vote/', question_view.vote, name='vote'),
+    path('question/<int:question_id>/vote/', question_view.vote, name='question.vote'),
 ]
