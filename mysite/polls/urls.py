00bb8f9d7ff0c6e46c407886df0991aa0305ff62
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-10-11 17:56:23 -0500

    Used linter

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,14 +1,12 @@
+'''Polls URLs'''
 from django.urls import path
-from .views import *
+from .views import home_view, question_view
 
 app_name = 'polls'
 urlpatterns = [
     path('', home_view.index, name='home.index'),
     path('question/', question_view.index, name='question_index'),
-    # ex: /polls/question/5/
     path('question/<int:question_id>/', question_view.detail, name='question_detail'),
-    # ex: /polls/question/5/results/
     path('question/<int:question_id>/results/', question_view.results, name='question_results'),
-    # ex: /polls/question/5/vote/
     path('question/<int:question_id>/vote/', question_view.vote, name='question_vote'),
 ]
